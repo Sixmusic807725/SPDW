e45088f374bf1866e560bfb6278ad2956c087287
Cx12 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-16 06:00:07 +0700

    Pack

---
+++ Pack.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#Apa Lu Mau Recode? Ini Buat Ddos Tolol
+#Dasar Anak Ngentod Yang Taunya Recode Aja:v
+#Rapi Kan Kodingan Gw Su!?!?
+
+clear
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+bl='\33[36;1m' #BlueLight
+echo $bl""
+figlet -f standard Pack Ddos
+echo "_____________________________________________________________"
+
+echo "Tools Pack Ddos On Termosss" | lolcat
+echo $g"Author : Mr.Cx12" 
+echo "Contact Admin : 081227443846" | lolcat
+echo $g"Recode? gw hapus semua data lu!"
+ echo "_____________________________________________________________"
+
+echo $bl" Login Dulu Biar Aman:V"
+echo ""
+
+sleep 3
+
+read -p "Username : " usr
+
+if [ $usr = "spdw00" ]
+then
+
+figlet -f standard Sukses! | lolcat
+
+else
+  figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh Pack.sh
+
+fi
+
+read -p "Password: " pwd
+
+if [ $pwd = "pakle12" ]
+then
+
+   figlet -f standard Sukses! | lolcat
+
+sleep 1
+
+else
+
+    figlet -f slant Gagal! | lolcat
+
+sleep 2
+
+sh Pack.sh
+
+fi
+
+echo $b "Selamat anda Menggunakan Tools Ini!! :)"
+
+sleep 1
+
+clear
+
+#Process Tools
+
+sh Pack1.sh
